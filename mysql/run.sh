81b6c52a1f0e3c12bc601e68c56a4d1e24c3a3a8
wgzhao <zhaoweiguo@haodou.com>
2015-11-16 10:59:59 +0800

    使用percona数据库

---
+++ mysql/run.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#mysql server
+if [ -n "$MYSQL_PASSWORD" ];then
+    passwd=$MYSQL_PASSWORD
+else
+    passwd='dbaas123'
+fi
+if [ -d /var/lib/mysql/mysql ];then
+        #has initialized,run it directly
+        #/usr/share/percona-server/mysql.server start
+    service mysql start
+else
+    #create defualt my.cnf
+    id=$(( $RANDOM  % 1024 ))
+    echo """[mysqld]
+log-bin=mysql-bin
+binlog_format = ROW
+server-id       = $id
+port     = 3306
+log-slave-updates = 1
+gtid_mode = ON
+enforce-gtid-consistency = 1
+    """ >/etc/my.cnf
+
+    #initilize first
+    mysql_install_db  --user=mysql
+    #/usr/share/percona-server/mysql.server start
+    service mysql start
+    #create password
+    if [ "x$MYSQL_SLAVE" = "x1" ] && [ "x$MYSQL_MASTER_IP" != 'x' ];then
+            #create slave
+            echo "create mysql slave"
+            mysql -e "change master to master_user='repl',master_password='repl123',master_host='$MYSQL_MASTER_IP',master_port=3306,master_auto_position=1"
+            mysql -e "start slave"
+    else
+			#create default replication account
+			mysql -e "grant replication on *.* to 'repl'@'%' identified by 'repl123'";
+      mysql -e "delete from mysql.user where User = ''"
+      mysql -e "update mysql.user set Password = password('$passwd');flush privileges;"
+    fi
+fi
+
+
+# php-fpm
+service php-fpm start
+
+# nginx
+service nginx start
+
+# sshd
+/usr/sbin/sshd -D
